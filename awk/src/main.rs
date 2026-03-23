ccec3e48bec9bac121e58e86d8a54c5b71c1cb0c
tsu-miki <wimps.r0930@icloud.com>
2026-03-23 20:32:25 +0900

    add awk basic command

--- awk/src/main.rs
+++ awk/src/main.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
+fn basic() {
+    let csv_file_path = "data/employees.csv";
+
+    println!("---- 行全体を出力 ----", );
+    let output = Command::new("awk")
+        .arg("{print $0}")
+        .arg(csv_file_path)
+        .output()
+        .expect("awk の実行に失敗");
+    
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    println!("{}", stdout);
+}
+
 fn main() {
-    println!("Hello, world!");
+    basic();
 }
